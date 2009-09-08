b323a5744362591adffc249b759a38e47d732031
shellfy <shellfy@a23f7426-7529-11de-86bc-2d44e85db88f>
2009-09-08 15:29:49 +0000

    添加显示宽字符功能

--- WindowsNetworking/LearnWNet/EnumFunction.cpp
+++ WindowsNetworking/LearnWNet/EnumFunction.cpp
@@ -3,15 +3,17 @@
 using namespace std;
 #include <winnetwk.h>
 
-#pragma comment(lib, "Mpr.lib");	//系统库，必须调用,WNetOpenEnum的函数要用
+#pragma comment(lib, "Mpr.lib")	//系统库，必须调用,WNetOpenEnum的函数要用
 
 bool enumFunction();
 bool enumFunction2();
-
+void log(LPTSTR msg, int len = 0);
 int main()
 {
 	enumFunction();
-
+	LPTSTR tmp = TEXT("小丑");
+	log(tmp, (int)wcslen(tmp));
+	system("pause");
 	return 0;
 }
 
@@ -40,7 +42,7 @@ bool enumFunction()
 			&cbBuffer);
 		if(dwResult == NO_ERROR){
 			for(int i = 0; i < int(cEntries); i++){
-				cout<<lpnrLocal[i].lpLocalName<<endl;
+				log(lpnrLocal[i].lpRemoteName);
 			}
 		}
 	}while(dwResult != ERROR_NO_MORE_ITEMS);
@@ -62,14 +64,14 @@ bool enumFunction2()
 		NULL,
 		&hEnum);
 	DWORD dwSize1=4096, dwCount1=-1;
-	dwResult = WNetEnumResource(hEnum, &dwCount1, buf, &dwSize1);
+	dwResult = WNetEnumResource(hEnum, &dwCount1, buf1, &dwSize1);
 	Res1 = (NETRESOURCE *)buf1;
 	for(DWORD i=0; i<dwCount1; i++, Res1++){
 		DWORD dwSize2=4096, dwCount2=-1;
 		dwResult = WNetOpenEnum(RESOURCE_GLOBALNET,
 			RESOURCETYPE_ANY,
 			RESOURCEUSAGE_CONTAINER,
-			Res,
+			Res1,
 			&hEnum);
 		dwCount2=-1;
 		///注意，dwCount的值要为-1，dwSize2要足够大，这个函数要求这样
@@ -86,7 +88,7 @@ bool enumFunction2()
 				&dwCount3, buf3, &dwSize3);
 			Res3 = (NETRESOURCE *)buf3;
 			for(DWORD k=0; k<dwCount3; k++, Res3++){
-				cout<<Res3->lpRemoteName<<endl;
+				log(Res3->lpRemoteName);
 			}
 		}
 	}
@@ -95,4 +97,21 @@ bool enumFunction2()
 	delete Res3;
 	WNetCloseEnum(hEnum);
 	return true;
+}
+
+
+void log(LPTSTR msg, int len)
+{
+	char sMsg [4096];
+	len = wcslen(msg);
+	int len2 = WideCharToMultiByte(0,
+		0,
+		msg,
+		len, 
+		sMsg, 
+		4096, 
+		0, 
+		NULL);
+	sMsg[len2 + 1] = '\0';
+	cout<<sMsg<<endl;
 }
